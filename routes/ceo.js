8ce645d3236f84c524a63ecb40c3a089c875b846
etnik2002 <etnikz2002@gmail.com>
2024-02-08 20:43:42 +0100

    edit routes

--- routes/ceo.js
+++ routes/ceo.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { ceoAccessToken } = require("../auth/auth");
-const { createCeo, login, getStats, deactivateAgency ,activateAgency,editObserver,getObserverById, addCity, getAllCities, deleteCity, getCeoById, confirmDebtPayment, getAllObservers, deleteObs, setNrOfSeatsNotification, sendBookingToEmail, getAllCitiesPagination, changePassword, changeEmail, changePin, importCitiesFromExcel} = require("../controllers/ceo-controller");
+const { createCeo, login, getStats, deactivateAgency ,activateAgency,editObserver,getObserverById, addCity, getAllCities, deleteCity, getCeoById, confirmDebtPayment, getAllObservers, deleteObs, setNrOfSeatsNotification, sendBookingToEmail, getAllCitiesPagination, changePassword, changeEmail, changePin, importCitiesFromExcel, editCity} = require("../controllers/ceo-controller");
 const { attachmentUpload, excelUpload } = require('../helpers/multer/multer');
 const { requestLimiter } = require("../auth/limiter");
 const apicache = require("apicache");
@@ -23,6 +23,8 @@ router.post('/observer/edit/:id', editObserver);
 router.post('/observer/delete/:id', deleteObs);
 
 router.get('/all-cities',cache('1 minutes'), getAllCities);
+
+router.post('/city/edit/:id', editCity)
 
 router.get('/all-cities-pagination', getAllCitiesPagination);
 
